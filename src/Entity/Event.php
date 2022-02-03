0bdf741531dcacc8cdcf425751b9653114ebf17e
Johnson Rodrigues <johnsonrodrigues19@gmail.com>
2022-02-03 20:27:13 -0300

    first commit

---
+++ src/Entity/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="ulid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UlidGenerator::class)
+     */
+    private Ulid $id;
+
+
+}
